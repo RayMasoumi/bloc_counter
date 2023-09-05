a4b1768050ef610b68e66e6c95aa24da2ee06d7f
Ray <Ray.masoumi80@gmail.com>
2023-09-05 10:11:01 +0330

    app router class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
-import 'package:bloc_counter/presentation/screens/home_screen.dart';
-import 'package:bloc_counter/presentation/screens/second_screen.dart';
-import 'package:bloc_counter/presentation/screens/third_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,17 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-// * make a private cubit to be accessible in this class only, so that we can send the same counter to each context(screen) in routes through 'BlocProvider.value':
-  final CounterCubit _counterCubit = CounterCubit();
-
-//! the BlocProvider.vale widget doesn't close the cubit!!!
-  // * we want the cubit to close after this screen is disposed, so:
-  @override
-  void dispose() {
-    _counterCubit.close();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,28 +21,6 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      routes: {
-        '/': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const HomeScreen(
-                title: 'Home Screen', color: Colors.blueAccent)),
-        '/second': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const SecondScreen(
-                title: 'Second Screen', color: Colors.redAccent)),
-        '/third': (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const ThirdScreen(
-                title: 'Third Screen', color: Colors.greenAccent)),
-      },
-// *since we're using the 'named routing' we don't need the home property anymore:
-      // home: BlocProvider<CounterCubit>(
-      //   create: (context) => CounterCubit(),
-      //   child: const HomeScreen(
-      //     title: 'Flutter Demo Home Page',
-      //     color: Colors.blueAccent,
-      //   ),
-      // ),
     );
   }
 }
