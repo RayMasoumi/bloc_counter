0eb5fa21f0d139525ceecc94ddb2d65925670e8e
Ray <Ray.masoumi80@gmail.com>
2023-09-05 10:22:58 +0330

    there are some problems with the sdk and packaging but the code itself id correct

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:bloc_counter/presentation/router/app_router.dart';
 import 'package:flutter/material.dart';
+
+//!!!!!!!!!!A BETTER WAY!!!!!!!!!!!!!!!!!!!!:
+//! we can use a single cubit/bloc for a whole app instead of passing its value(using BlocProvider.value) to each screen one by one
+//! instead>> we can access it globally:
+//# to do that>> we only need to wrap the MaterialApp widget inside a BlocProvider
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +18,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final AppRouter _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,6 +28,8 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      onGenerateRoute:
+          _appRouter.onGenerateRoute, //# we pass the function as an argument
     );
   }
 }
